0b110c0d90ba8da3cbb6c467f5d3a5ee678c67a5
ramonabreu-data <cdevabreu@gmail.com>
2022-12-13 22:47:52 -0300

    titulos get & post

---
+++ index.py
@@ -0,0 +1,26 @@
+from flask import Flask, make_response, jsonify, request
+from db import Titulos
+
+app = Flask('__name__')
+app.config['JSON_SORT_KEYS'] = False
+
+@app.route('/titulos', methods = ['GET'])
+def get_titulos():
+    return make_response(
+        jsonify(
+            identificador='Lista de títulos.',
+            titulos = Titulos)
+    )
+
+@app.route('/titulos', methods = ['POST'])
+def create_titulo():
+    titulo = request.json
+    Titulos.append(titulo)
+    return make_response(
+        jsonify(
+            valida='Título Cadastrado.',
+            titulo=titulo)
+    )
+
+
+app.run()
